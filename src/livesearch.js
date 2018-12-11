958735ddfa58096ac9fb6990e5c7d3505ac905a1
James Bell <Noxachron@gmail.com>
2018-12-11 16:14:10 -0500

    Firsties

---
+++ src/livesearch.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './livesearch.css';
+
+class LiveSearch extends Component {
+  render() {
+    return (
+      <div className="main">
+        Hello
+      </div>
+    );
+  }
+}
+
+export default LiveSearch;
